553d2e002f99de0de285d7522d16d08fe4afd06f
Gabe Glasco <gabeglasco@gmail.com>
2023-08-10 13:23:47 -0500

    added db sql

--- database.sql
+++ database.sql
@@ -1,2 +1,12 @@
 -- Don't forget to add your create table SQL 
 -- It is also helpful to include some test data
+
+CREATE TABLE "items" (
+	"id" serial primary key,
+	"name" varchar(80) not null,
+	"quantity" decimal not null,
+	"unit" varchar(20)
+);
+
+INSERT INTO items ("name", "quantity", "unit")
+VALUES ('green beans', '3', 'bushel');
