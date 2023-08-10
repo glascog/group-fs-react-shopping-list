9e5b7fb26cdfba1d8bdc50fd8ad96bb70afcc380
Gabe Glasco <gabeglasco@gmail.com>
2023-08-10 14:10:41 -0500

    app.jsx start

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,15 +1,43 @@
 import React from 'react';
-
+import { useState, useEffect } from 'react';
 import Header from '../Header/Header.jsx'
 import './App.css';
 
 
 function App() {
+    const [shoppingList, setShoppingList] = useState ([]);
+
+    
+    function handleSubmit() {
+
+    }
+    
     return (
         <div className="App">
             <Header />
             <main>
-                <p>Under Construction...</p>
+                <h1>Add Item</h1>
+                <form onSubmit={handleSubmit}>
+                <input type="text" placeholder='Item' />
+                <input type="text" placeholder='Quantity' />
+                <input type="text" placeholder='Unit' />   
+                <button type='submit'>Save</button>
+                </form>
+
+                <h1>Shopping List</h1>
+             <button type='reset'>Reset</button>
+             <button type='clear'>Clear</button>
+
+             <div>
+                {shoppingList.map(item => (
+                   <>
+                   {item.name}
+                   {item.quantity}
+                   {item.unit}
+                   </>
+                ))}
+             </div>
+
             </main>
         </div>
     );
